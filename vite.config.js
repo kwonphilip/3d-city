3f2374ef9825464c23c6b66a78f00a22b0399d69
beepboop <35923674+kwonphilip@users.noreply.github.com>
2026-05-02 21:04:37 -0400

    feat: github pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/3d-city/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
